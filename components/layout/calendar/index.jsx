efe14fc05e3a7c426a584df78cf1e50007d87613
Fox Tail <foxtail25@gmail.com>
2024-02-13 18:04:14 +0300

    calendar

---
+++ components/layout/calendar/index.jsx
@@ -0,0 +1,22 @@
+
+export default function Calendar() {
+
+	let calendar = '';
+
+	(function () {
+
+		calendar += '<table>';
+
+
+		return calendar
+	}())
+
+	return <>
+
+		<table>
+			<th>
+				<tr></tr>
+			</th>
+		</table>
+	</>
+}
