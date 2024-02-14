65a5131ccfaecaf793a541ea63377d05737d48f1
Fox Tail <foxtail25@gmail.com>
2024-02-14 17:58:53 +0300

    calendar

--- components/layout/aside_left/aside_left.jsx
+++ components/layout/aside_left/aside_left.jsx
@@ -1,6 +1,7 @@
 import aside_left_banner from "../../../data/aside_left_banner";
 import aside_left_menu from "../../../data/aside_left_menu";
 import BannerLeftAside from "../aside_left_banner/aside_left_banner";
+import Calendar from '../calendar';
 import Aside_menu_item from "../menuitem_aside/menuitem_aside";
 import al from './aside_left.module.scss';
 
@@ -14,11 +15,15 @@ export default function Aside_left() {
     // console.log(banners)
 
     return <div className={al.aside_left}>
+
+        <Calendar />
+
+        <a className={al.month} href='http://official.liart.ru/calendars/month.pdf' target='_blank'>Афиша на месяц</a>
+
         <nav className={al.aside_nav_menu}>
             {aside_menu}
         </nav>
 
-        <a className={al.month} href='http://official.liart.ru/calendars/month.pdf' target='_blank'>Афиша на месяц</a>
 
         {banners}
 
--- components/layout/calendar/index.jsx
+++ components/layout/calendar/index.jsx
@@ -1,22 +1,86 @@
+import { useEffect, useRef } from 'react';
+import cb from './calendar.module.scss';
 
 export default function Calendar() {
 
-	let calendar = '';
+	let arrDate = [];
+	let g_date;
 
-	(function () {
+	function getCalendarTable() {
 
-		calendar += '<table>';
+		let arrWeek = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
 
+		let calendar = document.createElement('table');
+		const tableName = document.createElement('caption')
+		tableName.textContent = `Календарь на ${g_date.toLocaleString('ru-Ru', { month: 'long' })}`
+		calendar.appendChild(tableName)
+
+		let th = document.createElement('thead');
+
+		for (let i = 0; i < arrWeek.length; i++) {
+			let td = document.createElement('td');
+			td.textContent = arrWeek[i];
+			th.appendChild(td);
+		}
+		calendar.appendChild(th);
+
+		let tb = document.createElement('tbody')
+
+		for (let i = 1; i <= 5; i++) {
+			let tr = document.createElement('tr')
+			for (let j = 0; j < arrWeek.length; j++) {
+				let td = document.createElement('td');
+				td.textContent = arrDate[7 * (i - 1) + j].date
+				tr.appendChild(td)
+			}
+			tb.appendChild(tr)
+		}
+		calendar.appendChild(tb)
 
 		return calendar
-	}())
+	}
 
-	return <>
+	function getArrDate(date) {
+		g_date = date
+		let thisMonth = date.getMonth();
+		let thisYear = date.getFullYear();
+		let firsWeekDayThisMonth = new Date(thisYear, thisMonth, 1).getDay();
+		if (firsWeekDayThisMonth == 0) {
+			firsWeekDayThisMonth = 7;
+		}
+		let lastDateThsMonth = new Date(thisYear, thisMonth + 1, 0).getDate();
+		console.log(lastDateThsMonth);
 
-		<table>
-			<th>
-				<tr></tr>
-			</th>
-		</table>
-	</>
+		for (let index = 0; index < 35; index++) {
+			if (index + 1 < firsWeekDayThisMonth) {
+				// let date = new Date(thisYear, thisMonth, (1 - firsWeekDayThisMonth + index + 1)).getDate();
+				// arrDate.push(date);
+				arrDate.push('');
+			} else if (index > lastDateThsMonth + firsWeekDayThisMonth - 2) {
+				// let date = new Date(thisYear, thisMonth, (35 - (35 - index - lastDateThsMonth) + thisMonth - 1)).getDate()
+				// let date = new Date(thisYear, thisMonth, index).getDate() - 2
+				// arrDate.push(date);
+				arrDate.push('')
+			} else {
+				arrDate.push({ date: index + 1 - firsWeekDayThisMonth + 1, month: thisMonth })
+			}
+		}
+
+		console.log(arrDate)
+
+	}
+
+	let calend_block = useRef();
+
+	useEffect(() => {
+		getArrDate(new Date(2024, 1, 1))
+		calend_block.current.innerHTML = '';
+		calend_block.current.appendChild(getCalendarTable());
+	}, [])
+
+	return <div className={cb.calend_block}>
+		<div className={cb.arrow_left} />
+		<div ref={calend_block} className={cb.calendar_contain} />
+		<div className={cb.arrow_right} />
+	</div>
 }
