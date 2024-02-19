b5e611a8718e924e457498b6b7f469e2aa65bd5b
Fox Tail <foxtail25@gmail.com>
2024-02-19 18:25:34 +0300

    незнаю что но надо

--- components/layout/CalendarPopUp/index.jsx
+++ components/layout/CalendarPopUp/index.jsx
@@ -4,6 +4,7 @@ export default function CalendarPopUp({ ...data }) {
 	const { setPopUp, ...popUp } = data;
 	console.log(popUp)
 	return <div className={popup.bg}>
+		{/* <div className='' */}
 		<button onClick={() => setPopUp(null)}>close</button>
 	</div>
 }
