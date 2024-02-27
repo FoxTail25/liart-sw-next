b39eccd1e717a2a8d90bd93a8a479904fbe27b4c
Fox Tail <foxtail25@gmail.com>
2024-02-27 13:15:09 +0300

    helen index

---
+++ components/layout/card_news/CardNews.js
@@ -0,0 +1,39 @@
+import Link from "next/link"
+import styles from './CardNews.module.scss'
+import { useEffect, useState } from "react"
+
+
+
+const CardNews = ({id, img, teaser}) => {
+  const [flag, setFlag] = useState()
+
+  useEffect(() => {
+    let url = window.location.href
+    if(url.includes('new_in_lib')){
+      setFlag(true)
+    }else{
+      setFlag(false)
+    }
+  }, [])
+
+  return (<>
+  {!flag &&<Link href = {`/news/id/${id}`} className={styles.box}>
+    <img className={styles.avatar_img} alt="teaser" src = {`https://liart.ru/media/uploads/newsavatars/${img}`}/>
+    <div className={styles.teaser}>
+        <div className={styles.teaser_news}>{teaser.length > 70 ? `${teaser.slice(0, 70)}...` : teaser}</div>
+        <div className={styles.goTo} >Подробнее...</div>
+    </div>
+  </Link>} 
+  {flag &&<Link href = {`/new_in_lib/getId/${id}`} className={styles.box}>
+    <img className={styles.img_cover} alt="teaser" src = {`https://liart.ru/media/uploads/newinlib/itemavatars/${img}`}/>
+    <div className={styles.teaser}>
+        <div className={styles.teaser_news}>{teaser}</div>
+        <div className={styles.goTo} >Подробнее...</div>
+    </div>
+  </Link>}
+  </>
+)
+  
+}
+
+export default CardNews
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,64 @@
 import Head from "next/head";
-// import Layout from "../components/layout";
+import { useEffect, useState } from "react";
+import CardNews from "../components/layout/card_news/CardNews";
+import styles from './index.module.scss'
 
+
+// export async function getServerSideProps() {
+//     let repo = 5
+//     // Fetch data from external API
+//     let res = await fetch('http://localhost:3000/api/hi').then(res => res.json())
+//     // console.log(res)
+//     // Pass data to the page via props
+//     return { props: { repo, res } }
+// }
 
 export async function getServerSideProps() {
-    let repo = 5
-    // Fetch data from external API
-    let res = await fetch('http://localhost:3000/api/hi').then(res => res.json())
-    // console.log(res)
-    // Pass data to the page via props
-    return { props: { repo, res } }
+    const result = await fetch(`${process.env.NEXT_PUBLIC_URL}api/news_query/1_ru`, {
+        method: 'GET',
+        "Content-Type": "application/json",
+    })
+    const res = await result.json()
+    return { props: { res } }
 }
 
 
 
-export default function Home({ repo, res }) {
+export default function Home({ res }) {
 
     // console.log('server!? NO it`s client!', repo, res)
 
+    const [news, setNews] = useState()
+
+
+    useEffect(() => {
+        setNews(res.news)
+    }, [])
 
     return <>
         <Head>
             <title>Новости РГБИ</title>
         </Head>
-        <p>home</p>
+        {/* <p>home</p>
         {
             repo && <>
                 <p>{repo}</p>
                 <p>{res.message}</p>
             </>
-        }
+        } */}
+
+        <div className={styles.wrapper} >
+            <div className={styles.title}>&nbsp;Новости</div>
+            <div className={styles.content}>
+                {news && news.map((one, index) => <CardNews
+                    key={index}
+                    id={one.id}
+                    img={one.avatar_img_name}
+                    teaser={one.teaser}
+                />)}
+                <a className={styles.all_news} href='/news/1_ru'> Остальные новости</a>
+            </div>
+        </div>
 
     </>
 }
